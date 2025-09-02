32226ed2152e69e32c1726850af8e65554540d28
RobinAntu <141601857+RobinAntu@users.noreply.github.com>
2025-09-02 15:46:10 +1000

    Update RequireAuth.jsx

--- src/auth/RequireAuth.jsx
+++ src/auth/RequireAuth.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
-
-export default function RequireAuth({ children }) {
-  const { user } = useAuth();
-  const location = useLocation();
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-  return children;
-}
+diff --git a/src/auth/RequireAuth.jsx b/src/auth/RequireAuth.jsx
+index a925263f32d226b80e17eaf274c4b2d6bcd56d53..d935dbae197b9eae9e6ce4a0eb45e7a97254cad3 100644
+--- a/src/auth/RequireAuth.jsx
++++ b/src/auth/RequireAuth.jsx
+@@ -1,12 +1,12 @@
+ import React from "react";
+ import { Navigate, useLocation } from "react-router-dom";
+-import { useAuth } from "../context/AuthContext";
++import { useAuth } from "./AuthContext";
+ 
+ export default function RequireAuth({ children }) {
+   const { user } = useAuth();
+   const location = useLocation();
+   if (!user) {
+     return <Navigate to="/login" state={{ from: location }} replace />;
+   }
+   return children;
+ }
